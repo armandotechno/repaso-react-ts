8348dad161adbc0683d63873359364e7fa4c122e
Armando Capriles <armandotechno12@gmail.com>
2023-02-27 13:41:49 -0400

    Tipos básicos en TS

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,18 @@
 
 export const TiposBasicos = () => {
-  return (
+  
+    const name: string = 'Armando';
+    const age: number = 21;
+    const isActive: boolean = true;
+
+    const tasks: string[] = ['Estudiar', 'Buscar la comida', 'Practicar todo lo aprendido'];
+
+    return (
     <>
         <h3>Tipos Básicos</h3>
+        { name }, { age }, { (isActive) ? 'está activo' : 'no está activo' }
+        <br />
+        { tasks.join(', ') }
     </>
   )
 }
