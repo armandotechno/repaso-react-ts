71a05ab85e938f7067612f87a502c7bfdc55655e
Armando Capriles <armandotechno12@gmail.com>
2023-02-27 16:42:16 -0400

    Componente Login - Antesala al useReducer

--- src/components/ContadorConHook.tsx
+++ src/components/ContadorConHook.tsx
@@ -2,7 +2,7 @@ import { useCounter } from "../hooks/useCounter"
 
 export const ContadorConHook = () => {
 
-    const { valor, acumular } = useCounter();
+    const { valor, acumular } = useCounter(100);
     
     return (
         <>
--- src/hooks/useCounter.tsx
+++ src/hooks/useCounter.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
-export const useCounter = () => {
+export const useCounter = ( inicial: number = 10 ) => {
   
-    const [valor, setValor] = useState(10)
+    const [valor, setValor] = useState(inicial)
 
     const acumular = ( numero: number ) => {
         setValor( valor + numero )
